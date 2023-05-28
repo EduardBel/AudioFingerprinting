0505ac0e3b61bd8cd601a657723971c835161831
EduardBel <edubel.98@gmail.com>
2023-05-28 14:29:17 +0200

    fingerprint almost

--- builddb.py
+++ builddb.py
@@ -1,11 +1,11 @@
 import sys
-from matplotlib import mlab
-# for data transformation 
-import numpy as np 
+
 # for visualizing the data 
-import matplotlib.pyplot as plt
 # for opening the media file 
 import scipy.io.wavfile as wavfile
+
+import fingerprint as fp
+
 
 def get_input_values():
     # Check if the correct number of arguments are provided
@@ -36,11 +36,11 @@ def get_input_values():
     return input_folder, output_file
 
 
-SAMPLE_RATE = 44100
-BITS_PER_SAMPLE = 16
-CHANNELS = 2
-window_size = 2**12
-window_overlap = int (window_size * 0.5)
+
+
+
+
+
 
 
 #main starts here
@@ -52,51 +52,19 @@ audio_file = 'library/02.FortheBetter.wav'
 
 _, channels = wavfile.read(audio_file) #get audio data
 channels = channels[:,0] # select left channel only
-
-spectrogram, freqs, times = mlab.specgram(channels,NFFT=window_size, Fs=SAMPLE_RATE, window=mlab.window_hanning, noverlap=window_overlap)
-
-spectrogram = 10 * np.log10(spectrogram)  # transmorm linear output to dB scale 
-# spectrogram[spectrogram == -np.inf] = 0  # replace infs with zeros
-# fig, axes = plt.subplots() 
-# axes.imshow(spectrogram) 
-# axes.set_xlabel('Time')
-# axes.set_ylabel('Frequency') 
-# axes.set_title( 'Spectrogram') 
-# plt.gca().invert_yaxis()
-# plt.show()
+#TODO: dual channel
+hashes=fp.generate_fingerprints(channels)
 
 
-block_size = 50  # Adjust this value?
-# Find points with higher amplitude within each block
-peak_points = []
-
-# Iterate over the spectrogram using block-wise steps
-for i in range(0, len(times), block_size):
-    for j in range(0, len(freqs), block_size):
-        # Get the block of amplitudes
-        block_amplitudes = spectrogram[j:j+block_size, i:i+block_size]
-        
-        # Find the maximum amplitude within the block
-        max_amplitude = np.max(block_amplitudes)
-        
-        # Find the indices of the maximum amplitude within the block
-        indices = np.where(block_amplitudes == max_amplitude)
-        
-        # Convert the indices to frequency and time coordinates
-        max_freqs = freqs[j + indices[0]]
-        max_times = times[i + indices[1]]
-        
-        # Store the peak point with maximum amplitude within the block
-        peak_points.extend(list(zip(max_freqs, max_times)))
 
 # Plot the spectrogram with peak points marked
-plt.figure()
-plt.imshow(spectrogram, origin='lower', aspect='auto', extent=[times.min(), times.max(), freqs.min(), freqs.max()])
-plt.colorbar(label='Amplitude (dB)')
-plt.scatter([p[1] for p in peak_points], [p[0] for p in peak_points], color='red', s=10, label='Peak Points')
-plt.xlabel('Time (s)')
-plt.ylabel('Frequency (Hz)')
-plt.title('Spectrogram with Peak Points')
-plt.legend()
-plt.show()
+# plt.figure()
+# plt.imshow(spectrogram, origin='lower', aspect='auto', extent=[times.min(), times.max(), freqs.min(), freqs.max()])
+# plt.colorbar(label='Amplitude (dB)')
+# plt.scatter([p[1] for p in peak_points], [p[0] for p in peak_points], color='red', s=10, label='Peak Points')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Frequency (Hz)')
+# plt.title('Spectrogram with Peak Points')
+# plt.legend()
+# plt.show()
 
---
+++ fingerprint.py
@@ -0,0 +1,64 @@
+from matplotlib import mlab
+import numpy as np 
+import hashlib
+
+
+def generate_fingerprint(channel):
+    window_size = 2**12 #should be a power of 2
+    window_overlap = int (window_size * 0.5)
+    #generate spectrogram
+    spectrogram, freqs, times = mlab.specgram(channel,NFFT=window_size, Fs=SAMPLE_RATE, window=mlab.window_hanning, noverlap=window_overlap)
+    spectrogram = 10 * np.log10(spectrogram)  # transmorm linear output to dB scale 
+    # spectrogram[spectrogram == -np.inf] = 0  # replace infs with zeros
+    peak_points=generate_robust_constellation(spectrogram, freqs, times)
+    hashes=fast_combinatorial_hashing(peak_points)
+    return hashes
+
+
+
+def generate_robust_constellation(spectrogram, freqs, times):
+    # Find points with higher amplitude within each block
+    peak_points = []
+
+    # Iterate over the spectrogram using block-wise steps
+    for i in range(0, len(times), BLOCK_SIZE):
+        for j in range(0, len(freqs), BLOCK_SIZE):
+            # Get the block of amplitudes
+            block_amplitudes = spectrogram[j:j+BLOCK_SIZE, i:i+BLOCK_SIZE]
+            
+            # Find the maximum amplitude within the block
+            max_amplitude = np.max(block_amplitudes)
+            
+            # Find the indices of the maximum amplitude within the block
+            indices = np.where(block_amplitudes == max_amplitude)
+            
+            # Convert the indices to frequency and time coordinates
+            max_freq = freqs[j + indices[0]]
+            max_time = times[i + indices[1]]
+            
+            # Store the peak point with maximum amplitude within the block
+            peak_points.extend(list(zip(max_freq, max_time)))
+    return peak_points
+
+
+def fast_combinatorial_hashing(peak_points):
+    hashes = []
+    for i in range(len(peak_points)):   #select anchor point
+        for j in range(1, TARGET_ZONE): #select points in target zone
+            if (i + j) < len(peak_points):  #if we are not out of range
+                time1 = peak_points[i][1]   #get times of anchor point and point in target zone
+                time2 = peak_points[i + j][1]
+                freq1 = peak_points[i][0]   #get frequencies of anchor point and point in target zone
+                freq2 = peak_points[i + j][0]
+                t_delta = time2 - time1 #calculate time difference
+                string_to_hash = f"{freq1}|{freq2}|{t_delta}"   #create a string to hash
+                # Generate a hash and add it to the list
+                hashes.append(hashlib.sha256(string_to_hash.encode()).hexdigest())
+    return hashes
+
+
+
+
+SAMPLE_RATE = 44100
+TARGET_ZONE = 8
+BLOCK_SIZE = 50  # Adjust this value?
